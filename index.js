9aa0b67f560a51b5c7a2e9c0317e0d01aed2366a
Eri4ka <navyhasanov@gmail.com>
2023-04-22 22:30:43 +0500

    Data structure 2.3, 2.4, 2.5

--- index.js
+++ index.js
@@ -79,3 +79,26 @@ function reverseArrayInPlace(arr) {
   }
 }
 
+// 2.3
+function arrayToList(arr) {
+  let list = null;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    list = {value: arr[i], rest: list};
+  }
+  return list;
+}
+
+// 2.4
+function listToArray(list) {
+  let array = [];
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+  return array;
+}
+
+// 2.5
+function prepend(value, list) {
+  return {value, rest: list};
+}
+
